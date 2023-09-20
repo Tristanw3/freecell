588605c4a794d6d500755d1c702d2f8663e64219
unknown <tristanw3@outlook.com>
2023-09-20 22:55:11 +1000

    add extra

--- app/functions.js
+++ app/functions.js
@@ -1,3 +1,61 @@
+const cardMapping = {
+    'c-10': '10_of_clubs',
+    'd-10': '10_of_diamonds',
+    'h-10': '10_of_hearts',
+    's-10': '10_of_spades',
+    'c-2': '2_of_clubs',
+    'd-2': '2_of_diamonds',
+// 2_of_hearts.svg
+// 2_of_spades.svg
+// 3_of_clubs.svg
+// 3_of_diamonds.svg
+// 3_of_hearts.svg
+// 3_of_spades.svg
+// 4_of_clubs.svg
+// 4_of_diamonds.svg
+// 4_of_hearts.svg
+// 4_of_spades.svg
+// 5_of_clubs.svg
+// 5_of_diamonds.svg
+// 5_of_hearts.svg
+// 5_of_spades.svg
+// 6_of_clubs.svg
+// 6_of_diamonds.svg
+// 6_of_hearts.svg
+// 6_of_spades.svg
+// 7_of_clubs.svg
+// 7_of_diamonds.svg
+// 7_of_hearts.svg
+// 7_of_spades.svg
+// 8_of_clubs.svg
+// 8_of_diamonds.svg
+// 8_of_hearts.svg
+// 8_of_spades.svg
+// 9_of_clubs.svg
+// 9_of_diamonds.svg
+// 9_of_hearts.svg
+// 9_of_spades.svg
+// ace_of_clubs.svg
+// ace_of_diamonds.svg
+// ace_of_hearts.svg
+// ace_of_spades.svg
+// black_joker.svg
+// jack_of_clubs.svg
+// jack_of_diamonds.svg
+// jack_of_hearts.svg
+// jack_of_spades.svg
+// king_of_clubs.svg
+// king_of_diamonds.svg
+// king_of_hearts.svg
+// king_of_spades.svg
+// queen_of_clubs.svg
+    'd-12':'queen_of_diamonds',
+    'h-12':'queen_of_hearts',
+    's-12':'queen_of_spades',
+    'r-14':'red_joker'
+
+}
+
 function createAllCardsList() {
     let allCards = [];
     let cardSuit = ['s', 'h', 'c', 'd'];
@@ -82,11 +140,14 @@ function stopDragOver(e) {e.preventDefault();}
 
 function createCard(cardValue) {
     let card = document.createElement('div');
-    card.innerText = cardValue['suit'] + '-' + cardValue['number'];
+    let cardImage = document.createElement('img');
+    let cardText = cardValue['suit'] + '-' + cardValue['number'];
+    cardImage.src = './card_images/' + cardMapping[cardText] + '.svg';
     card.className = 'card';
     card.dataset.suit = cardValue['suit'];
     card.dataset.colour = 'hd'.includes(cardValue['suit']) ? 'r' : 'b'
     card.dataset.number = cardValue['number'];
+    card.append(cardImage);
     return card
 }
 
